f699796c8d7d638133427eb9c7b9e3f8482e4099
Daniel da Rocha <danrocha@gmail.com>
2019-12-16 08:33:22 +0800

    implement base input and base button

---
+++ components/BaseButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    :disabled="loading"
+    class="p-2 text-sm font-bold tracking-wide text-yellow-900 uppercase bg-yellow-500 border border-yellow-600 rounded shadow"
+  >
+    <half-circle-spinner v-if="loading" :animation-duration="1000" :size="20" />
+    <slot v-if="!loading" />
+  </button>
+</template>
+
+<script>
+import { HalfCircleSpinner } from 'epic-spinners'
+export default {
+  components: {
+    HalfCircleSpinner
+  },
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/BaseInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+    @input="updateValue"
+    class="w-full p-2 border rounded shadow-inner focus:outline-none focus:border-yellow-500 disabled:text-gray-700 disabled:bg-gray-500"
+    type="text"
+  />
+</template>
+
+<script>
+export default {
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
