184289f46c000e2c1f07b364d9610bee2cf33656
Daniel da Rocha <danrocha@gmail.com>
2019-12-16 08:32:02 +0800

    add click outside directive

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~plugins/vue-js-modal'],
+  plugins: ['~plugins/vue-js-modal', '~plugins/v-click-outside'],
   /*
    ** Nuxt.js dev-modules
    */
