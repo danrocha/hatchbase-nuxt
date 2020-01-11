7684311d8dd8a2f0ea25172b94d1c097c7dc731b
Daniel da Rocha <danrocha@gmail.com>
2020-01-11 11:41:07 +0800

    refactor add card modal allow manual adding

--- components/CardAddModal.vue
+++ components/CardAddModal.vue
@@ -5,79 +5,104 @@
     </h2>
     <card-actions @success-add="showCardDetails" />
     <!-- FETCH FROM URL -->
-    <div v-if="!showForm">
-      <form class="mb-8" @submit.prevent="fetchJobDetails()">
-        <label class="block mb-2 text-gray-600" for="url"
-          >Paste the job ad's URL so we can fetch the details for you:</label
-        >
-        <p class="mb-2">
-          <el-input
-            v-model="url"
-            :disabled="loading"
-            placeholder="http://..."
-          />
-        </p>
-        <div class="flex mb-8">
-          <el-button type="primary" :loading="loading" native-type="submit"
+    <div v-if="!manual">
+      <el-form
+        ref="formAuto"
+        :model="formAuto"
+        :rules="rulesAuto"
+        class="mb-8"
+        @submit.native="submitForm('formAuto')"
+      >
+        <div class="mb-6">
+          <el-form-item
+            prop="url"
+            label="Paste the job ad's URL so we can fetch the details for you:"
+          >
+            <!-- <label class="block mb-2 text-gray-600" for="url"
+            >Paste the job ad's URL so we can fetch the details for you:</label
+          >-->
+            <el-input
+              v-model="formAuto.url"
+              :disabled="loading"
+              placeholder="http://..."
+            />
+          </el-form-item>
+        </div>
+        <el-form-item class="flex mb-8">
+          <el-button
+            type="primary"
+            :loading="loading"
+            @click="submitForm('formAuto')"
             >Add</el-button
           >
-          <button class="ml-4 text-gray-600 underline" @click="$emit('close')">
+          <el-button type="text" @click="$emit('close')">
             Cancel
-          </button>
-        </div>
-      </form>
+          </el-button>
+        </el-form-item>
+      </el-form>
       <p class="text-gray-600">
         Or
-        <button class="text-blue-500 underline" @click="showForm = true">
+        <button class="text-blue-500 underline" @click="manual = true">
           add details manually.
         </button>
       </p>
     </div>
     <div v-else>
-      <form>
-        <p class="mb-4">
-          <label class="block mb-2 text-gray-600" for="applicationTitle"
-            >What's the name of this application?</label
-          >
-          <base-input
-            v-model="details.title"
-            name="applicationTitle"
+      <el-form
+        ref="formManual"
+        :model="formManual"
+        :rules="rulesManual"
+        class="mb-8"
+      >
+        <el-form-item prop="title" label="What's the name of this application?">
+          <!-- <label class="block mb-2 text-gray-600" for="applicationTitle"
+              >What's the name of this application?</label
+            > -->
+          <el-input
+            v-model="formManual.title"
             placeholder="Junior Architect at OMA"
           />
-        </p>
+        </el-form-item>
         <div class="flex flex-wrap mb-4">
-          <p class="w-1/2 pr-4">
-            <label class="block mb-2 text-gray-600" for="officeName"
+          <el-form-item
+            class="w-1/2 pr-4"
+            prop="officeName"
+            label="What's the office's name?"
+          >
+            <!-- <label class="block mb-2 text-gray-600" for="officeName"
               >What's the office name?</label
-            >
-            <base-input
-              v-model="details.office"
-              name="officeName"
-              placeholder="OMA HK"
-            />
-          </p>
-          <p class="w-1/2">
-            <label class="block mb-2 text-gray-600" for="officeUrl"
+            > -->
+            <el-input v-model="formManual.officeName" placeholder="OMA HK" />
+          </el-form-item>
+
+          <el-form-item
+            class="w-1/2"
+            prop="officeUrl"
+            label="What's the office website?"
+          >
+            <!-- <label class="block mb-2 text-gray-600" for="officeUrl"
               >What's the office website?</label
-            >
-            <base-input
-              v-model="details.officeUrl"
-              name="officeUrl"
+            > -->
+            <el-input
+              v-model="formManual.officeUrl"
               placeholder="https://oma.eu/"
             />
-          </p>
+          </el-form-item>
         </div>
-        <div class="flex mb-8">
-          <base-button :loading="loading" type="submit" class="mr-4"
-            >Add</base-button
-          >
-          <button class="text-gray-600 underline" @click="$emit('close')">
-            Cancel
-          </button>
-        </div>
-      </form>
+        <el-form-item class="flex mb-8"> </el-form-item>
+        <el-button
+          :loading="loading"
+          type="primary"
+          class="mr-4"
+          @click="submitForm('formManual')"
+          >Add</el-button
+        >
+        <el-button type="text" @click="$emit('close')">
+          Cancel
+        </el-button>
+      </el-form>
       Or
-      <button class="text-blue-500 underline" @click="showForm = false">
+      <button class="text-blue-500 underline" @click="manual = false">
         go back to automatic.
       </button>
     </div>
@@ -85,13 +110,9 @@
 </template>
 
 <script>
-import BaseInput from '@/components/BaseInput'
 import CardActions from '@/components/CardActions'
-import BaseButton from '@/components/BaseButton'
 export default {
   components: {
-    BaseButton,
-    BaseInput,
     CardActions
   },
   props: {
@@ -106,21 +127,55 @@ export default {
   },
   data() {
     return {
-      url: '',
-      showForm: false,
+      formAuto: {
+        url: ''
+      },
+      rulesAuto: {
+        url: [
+          {
+            type: 'url',
+            required: true,
+            message: 'Please paste a URL',
+            trigger: 'blur'
+          }
+        ]
+      },
+      manual: false,
       loading: false,
-      details: { title: '', officeName: '', officeUrl: '' },
+      formManual: { title: '', officeName: '', officeUrl: '' },
+      rulesManual: {
+        title: [
+          {
+            required: true,
+            message:
+              'Please enter a name for you card (usually the job ad title)',
+            trigger: 'blur'
+          }
+        ],
+        officeName: [
+          {
+            required: true,
+            message: 'Please enter the name of the hiring office',
+            trigger: 'blur'
+          }
+        ],
+        officeUrl: [
+          {
+            type: 'url',
+            message: 'This does not seem to be a correct URL! Double check?',
+            trigger: 'blur'
+          }
+        ]
+      },
       loaded: false,
       loader: null
     }
   },
   methods: {
-    async fetchJobDetails() {
+    async fetchJobDetails(url) {
       console.log('Started fetching...')
-      this.loading = true
       this.$nuxt.$loading.start()
 
-      const url = this.url
       try {
         const details = await this.$axios.$get(
           `http://localhost:4000/get-job-info?url=${url}`
@@ -148,6 +203,37 @@ export default {
 
       const newCard = e
       this.$bus.$emit('modal-card-details', newCard)
+    },
+    submitForm(formName) {
+      this.loading = true
+
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          if (formName === 'formAuto') {
+            this.fetchJobDetails(this.formAuto.url)
+          } else {
+            console.log('adding manual details:', this.formManual)
+            this.addManualCard(this.formManual)
+          }
+        } else {
+          this.loading = false
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    addManualCard({ title, officeName, officeUrl }) {
+      const details = {
+        title,
+        officeName,
+        officeUrl,
+        listId: this.listId,
+        order: this.nextOrder
+      }
+      this.$bus.$emit('card-add', { card: { ...details } })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
     }
   }
 }
