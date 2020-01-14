65e59b74765821ba1b94e9a2bee630aeb5bf7040
Daniel da Rocha <danrocha@gmail.com>
2020-01-14 19:03:20 +0800

    add index assets

---
+++ components/Mailchimp.vue
@@ -0,0 +1,45 @@
+<template>
+  <form
+    id="mc-embedded-subscribe-form"
+    action="https://gmail.us5.list-manage.com/subscribe/post?u=2b5ac83e1245383a9c371ac4f&amp;id=9a426f23c5"
+    method="post"
+    name="mc-embedded-subscribe-form"
+    class="validate"
+    target="_blank"
+    novalidate
+  >
+    <div class="sm:flex">
+      <input
+        id="mce-EMAIL"
+        type="email"
+        value=""
+        name="EMAIL"
+        class="w-full h-12 px-4 mb-4 text-gray-900 border border-gray-500 rounded sm:mr-2"
+        placeholder="Enter your email"
+      />
+      <button
+        type="submit"
+        name="subscribe"
+        class="w-full h-12 font-bold text-yellow-900 bg-yellow-500 rounded shadow-lg hover:border hover:border-yellow-600 hover:bg-yellow-400"
+      >
+        Join the waitlist &rarr;
+      </button>
+    </div>
+
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true">
+      <input
+        type="text"
+        name="b_2b5ac83e1245383a9c371ac4f_9a426f23c5"
+        tabindex="-1"
+        value=""
+      />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
